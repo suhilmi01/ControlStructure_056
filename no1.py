28d56fb0449ef57ba023478984a32aea677aa989
suhilmi01 <suhilmi01@mail.com>
2026-09-22 11:26:10 +0700

    soal nomor 1

--- no1.py
+++ no1.py
@@ -2,3 +2,11 @@ persentase = int(input("masukkan persentase siswa): "))
 
 if persentase >=90:
     print("Exellent performance")
+elif persentase  >= 80:
+    print("Very Good performance")
+elif persentase >= 70:
+    print("Good performance")
+elif persentase >= 60:
+    print("average performance")
+else:
+    print("Performa di bawah standar rata-rata")
